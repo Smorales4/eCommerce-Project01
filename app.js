49cf51387247ad5e049b9072347b008cbddc89f1
Smorales4 <sofia.pmorales4@outlook.com>
2022-10-06 18:42:22 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// const imgBannerSection = document.querySelector('.imgBannerSection')
+// const slides = imgBannerSection.getElementsByTagName('')
+// var i = 0;
+
+// function nextSlide(){
+//     slides[i].classList.remove('active');
+//     i = (i + 1) % slides.length;
+//     slides[i].classList.add('active');
+// }
+
+// function prevSlide(){
+//     slides[i].classList.remove('active');
+//     i = (i + 1 + slides.length) % slides.length;
+//     slides[i].classList.add('active');
+// }
+
+if(document.querySelector('#slider')){
+    setInterval('fntExecuteSlide("next")',5000);
+ }
+
+
+
+ <div class='product-container'>
+ </div>
+
+ 
